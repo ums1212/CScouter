af37846b3416647a2a99b4ed2ba4f9ac5285655e
ums1212 <voll1212@gmail.com>
2025-12-01 12:30:57 +0900

    Chore: FaceMeasurementState 커밋 누락, bin폴더 ignore

---
+++ core/model/src/main/java/org/comon/model/FaceMeasurementState.kt
@@ -0,0 +1,18 @@
+package org.comon.model
+
+sealed class FaceMeasurementState {
+    object Idle : FaceMeasurementState()
+
+    data class Measuring(
+        val faceId: Int,
+        val startTimeMillis: Long,
+        val samples: List<Int>,
+        val boundingBox: FaceRect
+    ) : FaceMeasurementState()
+
+    data class Done(
+        val faceId: Int,
+        val averagedPower: Int,
+        val boundingBox: FaceRect
+    ) : FaceMeasurementState()
+}
